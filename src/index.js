1381535ded4188a6f41361de1df504ead70dab61
Joshua Russell <jkrussell756@gmail.com>
2018-11-05 12:11:40 -0500

    finished the tutorial. made some components. fully functioning redux app.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-// Import react and reactDOM, kinda hard to make this work without them
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import redux
+// Apparently this is all I'm doing?
 import index from "./js/index"
-
-// Import the App component
-import App from './App';
-
-// Import scss file
-import './scss/index.scss';
-
-// Tie the react script to the div build in the HTML template file.
-ReactDOM.render(<App />, document.getElementById('app'));
